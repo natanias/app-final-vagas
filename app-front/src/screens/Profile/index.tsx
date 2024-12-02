f0474a6ea5f2a9612d40306fa08649052e735d9d
nayran <nayran.nery@hotmail.com>
2024-12-01 21:00:35 -0300

    Atualizando rotas

--- app-front/src/screens/Profile/index.tsx
+++ app-front/src/screens/Profile/index.tsx
@@ -1,90 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { Alert } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React from 'react';
 import { Feather } from '@expo/vector-icons';
-import axios from 'axios';
-import {
+import { 
     Wrapper,
-    Container,
-    Header,
-    HeaderButtonContainer,
-    ButtonIcon,
+    Container, 
+    Header, 
+    HeaderButtonContainer, 
+    ButtonIcon, 
     ButtonText,
     ContentContainer,
 } from '../Profile/styles';
 import Logo from '../../components/Logo';
 import theme from '../../theme';
-import Input from '../../components/Input';
+import Input from '../../components/Input'
 import { Button } from '../../components/Button';
-import api from '../../services/api';
 
-export default function Profile({ navigation }) {
-    const [formData, setFormData] = useState({
-        id: '',
-        nome: '',
-        email: '',
-        password: '',
-    });
 
-    const fetchUserData = async () => {
-        try {
-            // Recupera o token JWT armazenado
-            const token = await AsyncStorage.getItem('userToken');
-            if (!token) {
-                Alert.alert('Erro', 'Token não encontrado. Faça login novamente.');
-                navigation.navigate('Login'); // Redireciona para login se não houver token
-                return;
-            }
-
-            const response = await api.get('usuarios/me', {
-                headers: { Authorization: `Bearer ${token}` },
-            });
-
-            setFormData(response.data);
-        } catch (error) {
-            console.error('Erro ao buscar dados do usuário:', error);
-            Alert.alert('Erro', 'Não foi possível carregar os dados do usuário.');
-        }
-    };
-
-    const handleInputChange = (field, value) => {
-        setFormData({ ...formData, [field]: value });
-    };
-
-    const [password, setPassword] = useState('');
-
-    const handlePasswordChange = (value) => {
-        setPassword(value);
-    };
-
-    const handleSave = async () => {
-        try {
-            // Recupera o token JWT armazenado
-            const token = await AsyncStorage.getItem('userToken');
-            if (!token) {
-                Alert.alert('Erro', 'Token não encontrado. Faça login novamente.');
-                navigation.navigate('Login');
-                return;
-            }
-
-            const updatedData = { ...formData };
-            if (password) {
-                updatedData.password = password;
-            }
-
-            await api.put(`usuarios/${formData.id}`, updatedData, {
-                headers: { Authorization: `Bearer ${token}` },
-            });
-
-            Alert.alert('Sucesso', 'Dados atualizados com sucesso!');
-        } catch (error) {
-            console.error('Erro ao atualizar os dados:', error);
-            Alert.alert('Erro', 'Não foi possível atualizar os dados.');
-        }
-    };
-    useEffect(() => {
-        fetchUserData();
-    }, []);
+export default function Profile({navigation }) {
 
     return (
         <Wrapper>
@@ -93,34 +24,25 @@ export default function Profile({ navigation }) {
                     <ButtonIcon>
                         <Feather size={16} name="chevron-left" color={theme.COLORS.BLUE} />
                     </ButtonIcon>
-                    <ButtonText>Voltar</ButtonText>
+                    <ButtonText>
+                        Voltar
+                    </ButtonText>
                 </HeaderButtonContainer>
                 <Logo />
             </Header>
 
             <Container>
                 <ContentContainer>
-                    <Input
-                        label="Nome"
-                        placeholder="Digite seu nome"
-                        value={formData.nome}
-                        onChangeText={(text) => handleInputChange('nome', text)}
-                    />
-                    <Input
-                        label="E-mail"
-                        placeholder="Digite seu e-mail"
-                        value={formData.email}
-                        onChangeText={(text) => handleInputChange('email', text)}
-                    />
+                    <Input label='Nome' placeholder='digite seu nome'/>
+                    <Input label='E-mail' placeholder='digite seu e-mail'/>
+                    <Input label='Senha' placeholder='digite sua senha'/>
                 </ContentContainer>
-                <Input
-                    label="Senha"
-                    placeholder="Digite sua senha"
-                    value={password}
-                    onChangeText={handlePasswordChange}
-                    secureTextEntry
-                />
-                <Button title="Salvar informações" noSpacing={true} variant="primary" onPress={handleSave} />
+
+                <Button 
+                    title="Salvar informações" 
+                    noSpacing={true} 
+                    variant='primary'
+                    />
             </Container>
         </Wrapper>
     );
